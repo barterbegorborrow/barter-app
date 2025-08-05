3e2fa3295d3d15e9764612d3b6c84750f25443a8
barterbegorborrow <jafenlivevideo@gmail.com>
2025-08-05 11:53:23 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Welcome to Barter App</h1>
+      <p>This is your starter page.</p>
+    </div>
+  );
+}

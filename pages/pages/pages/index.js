d39df39b03253293f05fd9009598ed646a6187a0
barterbegorborrow <jafenlivevideo@gmail.com>
2025-08-05 11:48:25 -0400

    Delete pages/pages/pages/index.js

--- pages/pages/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>Welcome to Barter App</h1>
-      <p>This is your starter page.</p>
-    </div>
-  );
-}

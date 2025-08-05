738399e5a50baa18ef43fee1f4a6c4d74a575538
barterbegorborrow <jafenlivevideo@gmail.com>
2025-08-05 12:12:33 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Welcome to BarterBegOrBorrow</h1>
+      <p>Your app is live!</p>
+    </div>
+  );
+}
